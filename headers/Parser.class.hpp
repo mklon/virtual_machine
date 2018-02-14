e84ea387975c4372bfcb889407ba2f69c424f684
mklon <mKlon9@gmail.com>
2018-02-14 16:43:49 +0200

    class Parser

---
+++ headers/Parser.class.hpp
@@ -0,0 +1,18 @@
+#ifndef PARSER_HPP
+# define PARSER_HPP
+
+#include "includes.hpp"
+#include "IOperand.hpp"
+
+class   Parser {
+private:
+    std::list<IOperand*>    _stack;
+
+
+
+
+
+public:
+};
+
+#endif
