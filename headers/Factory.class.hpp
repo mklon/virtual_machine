08986258769e7f04ecfaadec61af54b853346b6b
mklon <mKlon9@gmail.com>
2018-02-15 14:53:19 +0200

    factory method

--- headers/Factory.class.hpp
+++ headers/Factory.class.hpp
@@ -6,13 +6,22 @@
 
 class   Factory {
 private:
+    std::map<type, IOperand const * ( Factory::* )( std::string const & value ) const>   _method;
+
     IOperand const* createInt8( std::string const & value ) const;
     IOperand const* createInt16( std::string const & value ) const;
     IOperand const* createInt32( std::string const & value ) const;
     IOperand const* createFloat( std::string const & value ) const;
     IOperand const* createDouble( std::string const & value ) const;
 public:
+    Factory();
+    Factory( Factory const & rhs );
+
+    Factory   &operator=( Factory const & rhs );
+
     IOperand const * createOperand( type t, std::string const & value ) const;
+
+    ~Factory();
 };
 
 #endif
--- src/Factory.class.cpp
+++ src/Factory.class.cpp
@@ -1,25 +1,45 @@
 #include "../headers/Factory.class.hpp"
+#include "../headers/Operand.class.hpp"
+
+Factory::Factory() {
+    _method[INT8] = &Factory::createInt8;
+    _method[INT16] = &Factory::createInt16;
+    _method[INT32] = &Factory::createInt32;
+    _method[FLOAT] = &Factory::createFloat;
+    _method[DOUBLE] = &Factory::createDouble;
+}
+
+Factory::Factory(Factory const &rhs) {
+    *this = rhs;
+}
 
 IOperand const* Factory::createInt8(std::string const &value) const {
-
+    return ( new Operand<char>( INT8, value, 0 ) );
 }
 
 IOperand const* Factory::createInt16(std::string const &value) const {
-
+    return ( new Operand<char>( INT16, value, 0 ) );
 }
 
 IOperand const* Factory::createInt32(std::string const &value) const {
-
+    return ( new Operand<char>( INT32, value, 0 ) );
 }
 
 IOperand const* Factory::createFloat(std::string const &value) const {
-
+    return ( new Operand<char>( FLOAT, value, 7 ) );
 }
 
 IOperand const* Factory::createDouble(std::string const &value) const {
-
+    return ( new Operand<char>( DOUBLE, value, 14 ) );
 }
 
 IOperand const* Factory::createOperand(type t, std::string const &value) const {
-
+    return ( this->(( *_method.at( t ))( value )));
 }
+
+Factory&        Factory::operator=(Factory const &rhs) {
+    this->_method = rhs._method;
+    return ( *this );
+}
+
+Factory::~Factory() {}
